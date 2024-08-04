7dbd5f5179771b5c3765402fe40aecbf28f5eed9
KrustyKode <mlsineiro@gmail.com>
2024-08-04 12:23:07 -0700

    minmax

---
+++ minmax.py
@@ -0,0 +1,15 @@
+# Name:                         minmax.py
+# Author:                       Michael Sineiro
+# Date of latest revision:      8/4/2024
+# Purpose:                      find the min and max int inside of a list.
+
+# Define the list
+listA = [23, 12, 45, 22, 11, 78, 13, 90, 45, 88, 10, -5, -1]
+
+# Find the maximum and minimum values in the list
+max_value = max(listA)
+min_value = min(listA)
+
+# Display the results
+print(f"The maximum value in the list is: {max_value}")
+print(f"The minimum value in the list is: {min_value}")
